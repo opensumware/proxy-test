c29c961682c65fb00fcf52beff8b7e290a0a38f3
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2020-06-13 13:26:16 -0300

    - Proxy Test using JUnit

--- src/test/java/br/com/sumware/proxy/ProxyTestTest.java
+++ src/test/java/br/com/sumware/proxy/ProxyTestTest.java
@@ -1,10 +1,21 @@
 package br.com.sumware.proxy;
 
+import java.io.IOException;
+
+import org.apache.http.client.ClientProtocolException;
 import org.junit.jupiter.api.Test;
 
 public class ProxyTestTest {
     @Test
 	public void proxyTest() {
+		try {
+			String[] args = {"-h", "38.91.100.122", "-p", "3128", "-t", "https://www.terra.com.br"};
+			ProxyTest.main(args);
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		System.out.println("Test");
 	}
 }
